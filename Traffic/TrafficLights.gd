4840d4fda0de53bc9a5e800d62cb7485f4064c75
sejin <sejinjiji@gmail.com>
2026-04-21 07:57:59 +0900

    Feat 신호등 문제 setter 함수 사용 (#5) close #5

--- Traffic/TrafficLights.gd
+++ Traffic/TrafficLights.gd
@@ -8,9 +8,15 @@ extends Node2D
 enum Lights { RED, YELLOW, GREEN }
 
 
+var current_light : int = Lights.RED :
+	set(value):
+		current_light = value
+		lights_on(current_light)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	lights_on(Lights.RED)
+	lights_on(current_light)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,18 +31,16 @@ func lights_on(color : int) -> void:
 			yellow.set_visible(false)
 			green.set_visible(false)
 			await get_tree().create_timer(2.5).timeout
-			color = Lights.GREEN
+			current_light = Lights.GREEN
 		Lights.YELLOW:
 			red.set_visible(false)
 			yellow.set_visible(true)
 			green.set_visible(false)
 			await get_tree().create_timer(1.5).timeout
-			color = Lights.RED
+			current_light = Lights.RED
 		Lights.GREEN:
 			red.set_visible(false)
 			yellow.set_visible(false)
 			green.set_visible(true)
 			await get_tree().create_timer(0.5).timeout
-			color = Lights.YELLOW
-	
-	lights_on(color)
+			current_light = Lights.YELLOW
